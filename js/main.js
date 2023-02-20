38cddb02616481f78625e1ce5444f2d5f22cff62
Luiz <lhvalentim@outlook.coom>
2023-02-20 09:50:30 -0300

    disable translate

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-navigator.serviceWorker?.register('service-worker.js').then(reg => {
-	reg.addEventListener('updatefound', () => {
-		let newWorker = reg.installing
-		newWorker?.addEventListener('statechange', () => {
-			console.log('Update Installed. Restarting...')
-			if (newWorker.state == 'activated') location.reload(true)
-		})
-	})
-})
+navigator.serviceWorker?.register('service-worker.js')
+navigator.serviceWorker.onmessage = m => {
+	console.info('Update found!')
+	if (m?.data == 'update') location.reload(true)
+}
 
 function init() {
 	const initialColor = location.search.match(/(color=)([0-9a-f]{6})/i)
@@ -98,49 +94,69 @@ function init() {
 			blue: parseInt(blueInput.value || '0')
 		})
 	}
-}
 
-function refreshColor(color) {
-	if (!/^[0-9a-f]{6}$/i.test(color)) return ''
-	document.querySelector('#color').style.backgroundColor = `#${color}`
-}
-
-function lighten(color) {
-	if (!/^[0-9a-f]{6}$/i.test(color)) return ''
-	let rgb = hexToRgb(color)
-	rgb.red = parseInt(Math.max(10, rgb.red) + (rgb.red * 20 / 100))
-	if (rgb.red > 255) rgb.red = 255
-	rgb.green = parseInt(Math.max(10, rgb.green) + (rgb.green * 20 / 100))
-	if (rgb.green > 255) rgb.green = 255
-	rgb.blue = parseInt(Math.max(10, rgb.blue) + (rgb.blue * 20 / 100))
-	if (rgb.blue > 255) rgb.blue = 255
-	return rgbToHex(rgb)
-}
-
-function darken(color) {
-	if (!/^[0-9a-f]{6}$/i.test(color)) return ''
-	let rgb = hexToRgb(color)
-	rgb.red = parseInt(rgb.red - (rgb.red * 20 / 100))
-	if (rgb.red < 0) rgb.red = 0
-	rgb.green = parseInt(rgb.green - (rgb.green * 20 / 100))
-	if (rgb.green < 0) rgb.green = 0
-	rgb.blue = parseInt(rgb.blue - (rgb.blue * 20 / 100))
-	if (rgb.blue < 0) rgb.blue = 0
-	return rgbToHex(rgb)
-}
-
-function hexToRgb(hex) {
-	hex = '0x'+hex
-	return {
-		red: (hex >> 16) & 0xFF,
-		green: (hex >> 8) & 0xFF,
-		blue: hex & 0xFF
+	function refreshColor(color) {
+		if (!/^[0-9a-f]{6}$/i.test(color)) return ''
+		document.querySelector('#color').style.backgroundColor = `#${color}`
 	}
-}
 
-function rgbToHex(rgb) {
-	const color = (rgb.red << 16) | (rgb.green << 8) | rgb.blue
-	return color.toString(16).padStart(6, 0).toLocaleUpperCase()
+	function lighten(color, percentage=20) {
+		if (!/^[0-9a-f]{6}$/i.test(color)) return ''
+		let rgb = hexToRgb(color)
+		rgb.red = parseInt(Math.max(10, rgb.red) + (rgb.red * percentage/ 100))
+		if (rgb.red > 255) rgb.red = 255
+		rgb.green = parseInt(Math.max(10, rgb.green) + (rgb.green * percentage / 100))
+		if (rgb.green > 255) rgb.green = 255
+		rgb.blue = parseInt(Math.max(10, rgb.blue) + (rgb.blue * percentage / 100))
+		if (rgb.blue > 255) rgb.blue = 255
+		return rgbToHex(rgb)
+	}
+
+	function darken(color, percentage=20) {
+		if (!/^[0-9a-f]{6}$/i.test(color)) return ''
+		let rgb = hexToRgb(color)
+		rgb.red = parseInt(rgb.red - (rgb.red * percentage / 100))
+		if (rgb.red < 0) rgb.red = 0
+		rgb.green = parseInt(rgb.green - (rgb.green * percentage / 100))
+		if (rgb.green < 0) rgb.green = 0
+		rgb.blue = parseInt(rgb.blue - (rgb.blue * percentage / 100))
+		if (rgb.blue < 0) rgb.blue = 0
+		return rgbToHex(rgb)
+	}
+
+	function hexToRgb(hex) {
+		hex = '0x'+hex
+		return {
+			red: (hex >> 16) & 0xFF,
+			green: (hex >> 8) & 0xFF,
+			blue: hex & 0xFF
+		}
+	}
+
+	function rgbToHex(rgb) {
+		const color = (rgb.red << 16) | (rgb.green << 8) | rgb.blue
+		return color.toString(16).padStart(6, 0).toLocaleUpperCase()
+	}
+
+	function exportAngularMaterialTheme() {
+		const theme = `(
+	50: #${lighten(hexInput.value, 75)},
+	100: #${lighten(hexInput.value, 60)},
+	200: #${lighten(hexInput.value, 45)},
+	300: #${lighten(hexInput.value, 30)},
+	400: #${lighten(hexInput.value, 15)},
+	500: #${hexInput.value},
+	600: #${darken(hexInput.value, 20)},
+	700: #${darken(hexInput.value, 40)},
+	800: #${darken(hexInput.value, 60)},
+	900: #${darken(hexInput.value, 80)},
+	A100: #${lighten(hexInput.value, 90)},
+	A200: #${lighten(hexInput.value, 80)},
+	A400: #${lighten(hexInput.value, 70)},
+	A700: #${lighten(hexInput.value, 60)}
+);`
+	}
+
 }
 
 document.onreadystatechange = () => {
--- service-worker.js
+++ service-worker.js
@@ -3,12 +3,24 @@ self.addEventListener('install', event => {
 })
 
 self.addEventListener('fetch', event => {
-	event.respondWith(caches.open('declaracao-de-conteudo').then(cache => {
+	event.respondWith(caches.open('webgl').then(cache => {
 		return cache.match(event.request)
 		.then(cachedResponse => {
+			let cachedFile = cachedResponse.clone().blob()
 			const fetchedResponse = fetch(event.request)
 			.then(networkResponse => {
-				cache.put(event.request, networkResponse.clone())
+				if (networkResponse.status == 200) cache.put(event.request, networkResponse.clone())
+				Promise.all([
+					cachedFile,
+					networkResponse.clone().blob()
+				]).then(response => {
+					if (!event.clientId) return
+					if (response[0].size == response[1].size) return
+					self.clients.get(event.clientId)
+					.then(client => {
+						client?.postMessage('update')
+					})
+				})
 				return networkResponse
 			})
 			return cachedResponse || fetchedResponse
